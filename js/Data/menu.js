5c5f23093f09b73ef6288578a00c5ad928593be5
Zorthon <grondincam9@gmail.com>
2024-03-06 23:26:11 -0500

    stuff

---
+++ js/Data/menu.js
@@ -0,0 +1,28 @@
+var menuData ={
+    info:{
+        src:'images/MainMenuLoop.png'
+    },
+    states:{
+        idle:{
+            fps:15,
+            cycle:true,
+            frames:
+            [
+                {width:1024, height:512, startX:0, statY:0},
+                {width:1024, height:512, startX:1*1024, startY:0},
+                {width:1024, height:512, startX:2*1024, startY:0},
+                {width:1024, height:512, startX:3*1024, startY:0},
+                {width:1024, height:512, startX:4*1024, startY:0},
+                {width:1024, height:512, startX:5*1024, startY:0},
+                {width:1024, height:512, startX:6*1024, startY:0},
+                {width:1024, height:512, startX:7*1024, startY:0},
+                {width:1024, height:512, startX:8*1024, startY:0},
+                {width:1024, height:512, startX:9*1024, startY:0},
+                {width:1024, height:512, startX:10*1024, startY:0},
+                {width:1024, height:512, startX:11*1024, startY:0},
+                {width:1024, height:512, startX:12*1024, startY:0}
+            ]
+        }
+    }
+
+}
